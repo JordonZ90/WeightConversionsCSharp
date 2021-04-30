5b10665c37d5c0cae6869b531d846439770c8ba7
Vik <jzenterprize90@gmail.com>
2021-04-30 02:00:25 -0400

    CSharp version of weigh conversions program

---
+++ WeightConvertor/WeightConvertor/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace WeightConvertor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+            WeightConvertor();
+        }
+        public static void Menu()
+        {
+            Console.WriteLine("MENU");
+            Console.WriteLine("1. Pounds to Stone");
+            Console.WriteLine("2. Stone to Pounds");
+            Console.WriteLine("3. Pounds to Kilograms");
+            Console.WriteLine("4. Kilograms to Pounds");
+            Console.WriteLine("5. Kilograms to Stone");
+            Console.WriteLine("6. Stone to Kilograms");
+            Console.WriteLine("7. Ounces to Grams");
+            Console.WriteLine("8. Grams to Ounces");
+            Console.WriteLine("");
+        }
+        public static void WeightConvertor()
+        {
+            string again = "y";
+            while (again == "y".ToLower())
+            {
+                Console.WriteLine("Enter a menu option ");
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                if (option == 1)
+                {
+                    Console.WriteLine("Enter weight in Pounds");
+                    double pounds = Convert.ToDouble(Console.ReadLine());
+                    double stone = WeightMethods.PoundsToStone(pounds);
+                    stone = Math.Round(stone, 2);
+                    Console.WriteLine($"Weight in {stone} Stone");
+                }
+                else if (option == 2)
+                {
+                    Console.WriteLine("Enter weight in Stone");
+                    double stone = Convert.ToDouble(Console.ReadLine());
+                    double pounds = WeightMethods.StoneToKilograms(stone);
+                    pounds = Math.Round(pounds, 2);
+                    Console.WriteLine($"Weight in {pounds} Pounds");
+                }
+                else if (option == 3)
+                {
+                    Console.WriteLine("Enter weight in Pounds");
+                    double pounds = Convert.ToDouble(Console.ReadLine());
+                    double kilograms = WeightMethods.PoundsToKilograms(pounds);
+                    kilograms = Math.Round(kilograms, 2);
+                    Console.WriteLine($"Weight in {kilograms} Kilograms");
+
+                }
+                else if (option == 4)
+                {
+                    Console.WriteLine("Enter weight in Kilograms");
+                    double kilograms = Convert.ToDouble(Console.ReadLine());
+                    double pounds = WeightMethods.KiloGramsToPounds(kilograms);
+                    pounds = Math.Round(pounds, 2);
+                    Console.WriteLine($"Weight in {pounds} Pounds");
+                }
+                else if (option == 5)
+                {
+                    Console.WriteLine("Enter weight in Kilograms");
+                    double kilograms = Convert.ToDouble(Console.ReadLine());
+                    double stone = WeightMethods.KilogramsToStone(kilograms);
+                    stone = Math.Round(stone, 2);
+                    Console.WriteLine($"Weight in {stone} Stone");
+                }
+                else if (option == 6)
+                {
+                    Console.WriteLine("Enter weight in Stone");
+                    double stone = Convert.ToDouble(Console.ReadLine());
+                    double kilograms = WeightMethods.StoneToKilograms(stone);
+                    kilograms = Math.Round(kilograms, 2);
+                    Console.WriteLine($"Weight in {kilograms} Kilograms");
+                }
+                else if (option == 7)
+                {
+                    Console.WriteLine("Enter weight in Ounces");
+                    double ounces = Convert.ToDouble(Console.ReadLine());
+                    double grams = WeightMethods.OuncesToGrams(ounces);
+                    grams = Math.Round(grams, 2);
+                    Console.WriteLine($"Weight in {grams} Grams");
+                }
+                else if (option == 8)
+                {
+                    Console.WriteLine("Enter weight in Grams");
+                    double grams = Convert.ToDouble(Console.ReadLine());
+                    double ounces = WeightMethods.GramsToOunces(grams);
+                    ounces = Math.Round(ounces, 2);
+                    Console.WriteLine($"Weight in {ounces} Ounces");
+                }
+                else
+                {
+                    Console.WriteLine("You must enter a valid menu number");
+                }
+
+                Console.WriteLine("Convert another measurement? (y|n)");
+                again = Console.ReadLine();
+                if (again == "y")
+                {
+                    Menu();
+                }
+                else
+                {
+                    Console.WriteLine("Goodbye!");
+                }
+            }
+        }
+    }
+}
---
+++ WeightConvertor/WeightConvertor/WeightMethods.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeightConvertor
+{
+    public class WeightMethods
+    {
+        public static double PoundsToStone(double pounds)
+        {
+            double stone = pounds / 14;
+            return stone;
+        }
+        public static double StoneToPounds(double stone)
+        {
+            double pounds = stone * 14;
+            return pounds;
+        }
+        public static double PoundsToKilograms(double pounds)
+        {
+            double kilograms = pounds * 0.45359237;
+            return kilograms;
+        }
+        public static double KiloGramsToPounds(double kilograms)
+        {
+            double pounds = kilograms * 2.2046;
+            return pounds;
+        }
+        public static double KilogramsToStone(double kilograms)
+        {
+            double stone = kilograms / 6.35029318;
+            return stone;
+        }
+        public static double StoneToKilograms(double stone)
+        {
+            double kilograms = stone * 6.35029318;
+            return kilograms;
+        }
+        public static double OuncesToGrams(double ounces)
+        {
+            double grams = ounces * 28.3495231;
+            return grams;
+        }
+        public static double GramsToOunces(double grams)
+        {
+            double ounces = grams / 28.3495231;
+            return ounces;
+        }
+    }
+}
